74b92c1b2901dc577dd9529f2a798cf7c9a77172
shomrkm <shochan1985@gmail.com>
2022-04-10 11:16:02 +0900

    feat: Add base of the api.

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+// Route files
+const bootcamps = require('./routes/bootcamps')
+
+// Load env vars
+dotenv.config({ path: './config/.env' })
+
+const app = express();
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+});
