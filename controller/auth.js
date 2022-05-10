a663778c8e26b205f6619b65b09b0f2a5f490a7f
shomrkm <shochan1985@gmail.com>
2022-05-10 20:30:44 +0900

    feat: Sign and Get JWT in the resister request.

--- controller/auth.js
+++ controller/auth.js
@@ -16,7 +16,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   })
 
-  res.status(200).json({
-    success: true,
-  });
-})
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({ success: true, token });
+});
--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const UserScheme = new mongoose.Schema({
   name: {
@@ -12,10 +13,10 @@ const UserScheme = new mongoose.Schema({
     unique: true,
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-      'Please add a valid email'
+      'Please add a valid email',
     ],
   },
-  role : {
+  role: {
     type: String,
     enum: ['user', 'publisher'],
     default: 'user',
@@ -35,9 +36,16 @@ const UserScheme = new mongoose.Schema({
 });
 
 // Encrypt password using bcrypt
-UserScheme.pre('save', async function(next){
+UserScheme.pre('save', async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Sign JWT and return
+UserScheme.methods.getSignedJwtToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 module.exports = mongoose.model('User', UserScheme);
