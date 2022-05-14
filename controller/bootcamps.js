95ba4d4e22bdbc9ea1f7dca519e1f9e9518bf437
shomrkm <shochan1985@gmail.com>
2022-05-14 16:16:23 +0900

    feat: Add user id to Bootcamp model.

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -28,7 +28,17 @@ exports.getBootcamp = asyncHandler(async (req, res, next) => {
 // @route POST /api/v1/bootcamps
 // @access Private
 exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  req.body.user = req.user.id;
+
+  // Check for published bootcamp
+  // If the user is not an admin, they can only add one bootcamp
+  const publishedBootcamp = await Bootcamp.findOne({ user: req.user.id });
+  if(publishedBootcamp && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`The user with ID ${req.user.id} has already published bootcamp`, 400));
+  }
+
   const bootcamp = await Bootcamp.create(req.body);
+
   res.status(201).json({
     success: true,
     data: bootcamp,
