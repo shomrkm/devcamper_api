229b5443b3e6c7dbc26fc023ddb18e509a14c0d5
shomrkm <shochan1985@gmail.com>
2022-05-14 15:03:32 +0900

    feat: Implement role authorization.

--- routes/courses.js
+++ routes/courses.js
@@ -5,7 +5,7 @@ const {
 
  const Course = require('../models/Courses');
  const advancedResults = require('../middleware/advancedResults');
-const { protect } = require('../middleware/auth');
+const { protect, authorize } = require('../middleware/auth');
 
  const router = express.Router({ mergeParams: true});
 
@@ -14,11 +14,11 @@ const { protect } = require('../middleware/auth');
       path: 'bootcamp',
       select: 'name description'
     }), getCourses)
-  .post(protect, addCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
 
  router.route('/:id')
   .get(getCourse)
-  .put(protect, updateCourse)
-  .delete(protect, deleteCourse);
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
  module.exports = router;
