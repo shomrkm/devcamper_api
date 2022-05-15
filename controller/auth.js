3810b5fae23c1de8d08f386a84c75c8db333a72d
shomrkm <shochan1985@gmail.com>
2022-05-15 10:27:45 +0900

    feat: Add function to send a password reset email.

--- controller/auth.js
+++ controller/auth.js
@@ -1,5 +1,6 @@
 const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
+const sendEmail = require('../utils/sendEmail');
 const User = require('../models/Users');
 
 // @desc Resister user
@@ -92,8 +93,25 @@ exports.forgotPassword = asyncHandler(async (req, res, next) => {
 
   await user.save();
 
-  res.status(200).json({
-    success: true,
-    data: user,
-  });
+  // Create reset url
+  const resetUrl = `${req.protocol}://${req.get('host')}/api/v1/resetpassword/${resetToken}`;
+  const message = `Your are receiving this email because you (or someone else) has requested the reset of a password. Please make a PUT request to \n\n ${resetUrl}`;
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: 'Password reset token',
+      message,
+    });
+
+    res.status(200).json({ success: true, data: 'Email sent'});
+  } catch (err) {
+    console.log(err);
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save({ validateVeforeSave: false });
+
+    return next(new ErrorResponse('Email could not be sent', 500));
+  }
 });
