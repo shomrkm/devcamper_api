858c1485628bd260ebf7ff559de879d82ed5fe40
shomrkm <shochan1985@gmail.com>
2022-05-15 09:05:03 +0900

    feat: Implement permission checks to update/delete a bootcamp.

--- controller/bootcamps.js
+++ controller/bootcamps.js
@@ -49,14 +49,21 @@ exports.createBootcamp = asyncHandler(async (req, res, next) => {
 // @route PUT /api/v1/bootcamps/:id
 // @access Private
 exports.updateBootcamp = asyncHandler(async (req, res, next) => {
-  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  })
+  let bootcamp = await Bootcamp.findById(req.params.id)
 
   if(!bootcamp){
     return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
   }
+
+  // Make sure user is bootcmap owner
+  if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401));
+  }
+
+  bootcamp = await Bootcamp.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
 
   res.status(200).json({ success: true, data: bootcamp });
 });
@@ -69,6 +76,11 @@ exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
 
   if(!bootcamp){
     return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+  }
+
+  // Make sure user is bootcmap owner
+  if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.params.id} is not authorized to delete this bootcamp`, 401));
   }
 
   bootcamp.remove();
@@ -111,6 +123,11 @@ exports.bootcampPhotoUpload = asyncHandler(async (req, res, next) => {
 
   if(!bootcamp){
     return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+  }
+
+   // Make sure user is bootcmap owner
+  if(bootcamp.user.toString() !== req.user.id && req.user.role !== 'admin') {
+    return next(new ErrorResponse(`User ${req.params.id} is not authorized to update this bootcamp`, 401));
   }
 
   if(!req.files) {
