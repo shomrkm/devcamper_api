853bcd476dcbb064713d3b96dccd52b98b386269
shomrkm <shochan1985@gmail.com>
2022-05-18 16:17:50 +0900

    feat: Set token from cookie.

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,15 +9,17 @@ exports.protect = asyncHandler(async (req, res, next) => {
   let token;
 
   if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+    // Set token from Bearer token in header
     token = req.headers.authorization.split(' ')[1];
   }
-  // else if(req.cookies.token) {
-  //   token = req.cookies.token;
-  // }
+  else if(req.cookies.token) {
+    // Set token from cookie
+    token = req.cookies.token;
+  }
 
   // Make sure token is exists
   if(!token){
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 
   // Verify token
@@ -26,7 +28,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
     req.user = await User.findById(decoded.id);
     next();
   } catch (err) {
-    return next(new ErrorResponse('Not authorize to access this route', 401));
+    return next(new ErrorResponse('Not authorized to access this route', 401));
   }
 });
 
